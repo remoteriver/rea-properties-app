265f431cd7c74c14273bd56417e322b921016f35
remoteriver <remoteriver@gmail.com>
2022-11-07 21:24:08 +1100

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Homepage from "./pages/HomePage/HomePage";
+
+const App = () => {
+  return <Homepage />;
+};
+
+export default App;
---
+++ src/components/Flex/Flex.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import { BoxProps } from "@mui/system";
+
+const Flex = (props: BoxProps) => {
+  return <Box {...props} sx={{ display: "flex" }}></Box>;
+};
+
+export default Flex;
