9638a8309d2438834e9aacabb29506c11b38ce09
remoteriver <remoteriver@gmail.com>
2022-11-07 21:47:59 +1100

    Display PropertyCard on Homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,25 @@
+import { Container, Box, Typography } from "@mui/material";
 import React from "react";
+import Flex from "../../components/Flex/Flex";
+import PropertyCard from "../../components/PropertyCard/PropertyCard";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <Container>
+      <Flex>
+        <Box flex={1} px={2}>
+          <Typography>Results</Typography>
+          <Box>
+            <PropertyCard />
+          </Box>
+        </Box>
+        <Box flex={1}>
+          <Typography>Saved Properties</Typography>
+          <Box></Box>
+        </Box>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Homepage;
