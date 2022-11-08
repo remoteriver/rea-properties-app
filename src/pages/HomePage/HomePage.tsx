c8d269ca615ba4b159d17069d247194ec2da7f5b
remoteriver <remoteriver@gmail.com>
2022-11-08 19:37:13 +1100

    Load data.json in homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,16 +2,20 @@ import { Container, Box, Typography } from "@mui/material";
 import React from "react";
 import Flex from "../../components/Flex/Flex";
 import PropertyCard from "../../components/PropertyCard/PropertyCard";
+import data from "../../data/data.json";
+
+console.log(data);
 
 const Homepage = () => {
+  const properties = data.results;
   return (
     <Container>
       <Flex>
         <Box flex={1} px={2}>
           <Typography>Results</Typography>
-          <Box>
-            <PropertyCard />
-          </Box>
+          {properties.map((property) => (
+            <PropertyCard property={property} />
+          ))}
         </Box>
         <Box flex={1}>
           <Typography>Saved Properties</Typography>
