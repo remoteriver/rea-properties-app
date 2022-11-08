d66dc8e56f97dcaf646ba966db1e09e883d7f9cb
remoteriver <remoteriver@gmail.com>
2022-11-08 19:36:44 +1100

    Add stylings into PropertyCard component

--- src/components/PropertyCard/PropertyCard.tsx
+++ src/components/PropertyCard/PropertyCard.tsx
@@ -1,25 +1,38 @@
 import { Box, Card, Typography } from "@mui/material";
 import { BoxProps } from "@mui/system";
+import { Property } from "../../types/Property";
 import Flex from "../Flex/Flex";
 
-interface Props {}
+const placeholderImgSrc =
+  "https://via.placeholder.com/560x240?text=placeholder";
+
+interface Props {
+  property: Property;
+}
 
 const PropertyCard = (props: Props) => {
+  const { property } = props;
   return (
-    <Box my={2} boxShadow={1} borderRadius={2} height="350px">
-      <Flex height="40px" justifyContent="space-between">
-        <Box>Logo</Box>
-        <Box>BG</Box>
+    <Box my={2} boxShadow={1} borderRadius={2} overflow="hidden">
+      <Flex
+        height="36px"
+        justifyContent="space-between"
+        bgcolor={property.agency.brandingColors.primary}
+        p={1}
+      >
+        <Box>
+          <img height="100%" width="100%" src={property.agency.logo} />
+        </Box>
       </Flex>
-      <Box height="240px">
+      <Box height="360px">
         <img
           height="100%"
           width="100%"
-          src="https://via.placeholder.com/560x240?text=placeholder"
+          src={property.mainImage || placeholderImgSrc}
         />
       </Box>
-      <Box>
-        <Typography>Price</Typography>
+      <Box height="50px" p={1}>
+        <Typography>{property.price}</Typography>
       </Box>
     </Box>
   );
