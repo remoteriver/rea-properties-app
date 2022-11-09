0d9dc7cc73ab885dc3fcff07fb4b052dd1509f8e
remoteriver <remoteriver@gmail.com>
2022-11-09 12:48:19 +1100

    Add more test cases in PropertyCard.test to cover the button behaviours

--- src/components/PropertyCard/PropertyCard.test.tsx
+++ src/components/PropertyCard/PropertyCard.test.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import PropertyCard from './PropertyCard';
 import { Property } from 'types/Property';
 
@@ -14,12 +13,14 @@ describe('PropertyCard', () => {
 
     test('it renders', () => {
       render(<PropertyCard property={mockProperty} />);
+
       expect(screen.getByTestId('PROPERTY_CARD')).toBeInTheDocument();
       expect(screen.getByText('price')).toBeInTheDocument();
     });
 
     test('it renders with correct border styles', () => {
       render(<PropertyCard property={mockProperty} />);
+
       expect(screen.getByTestId('PROPERTY_CARD')).toBeInTheDocument();
       expect(screen.getByTestId('PROPERTY_CARD')).toHaveStyle({
         'box-shadow':
@@ -44,19 +45,53 @@ describe('PropertyCard', () => {
 
     test('it renders logo', () => {
       render(<PropertyCard property={mockProperty} />);
+
       expect(screen.getByAltText('logo')).toBeInTheDocument();
       expect(screen.getByAltText('logo')).toHaveAttribute('src', 'logo_src');
     });
 
     test('it renders main img', () => {
       render(<PropertyCard property={mockProperty} />);
+
       expect(screen.getByAltText('main')).toBeInTheDocument();
       expect(screen.getByAltText('main')).toHaveAttribute('src', 'main_src');
     });
 
     test('it renders price tag', () => {
       render(<PropertyCard property={mockProperty} />);
+
       expect(screen.getByText('$102,000')).toBeInTheDocument();
+    });
+
+    test('it renders the button when hover on', () => {
+      render(<PropertyCard property={mockProperty} mode="add" />);
+
+      fireEvent.mouseOver(screen.getByTestId('PROPERTY_CARD'));
+      expect(screen.getByText('Add Property')).toBeInTheDocument();
+    });
+
+    test('it renders the button when hover on', () => {
+      render(<PropertyCard property={mockProperty} mode="remove" />);
+
+      fireEvent.mouseOver(screen.getByTestId('PROPERTY_CARD'));
+      expect(screen.getByText('Remove Property')).toBeInTheDocument();
+    });
+
+    test('it triggers callback when the button is clicked', async () => {
+      const mockHandler = jest.fn();
+
+      render(
+        <PropertyCard
+          property={mockProperty}
+          mode="remove"
+          onButtonClicked={mockHandler}
+        />
+      );
+
+      fireEvent.mouseOver(screen.getByTestId('PROPERTY_CARD'));
+      await waitFor(() => screen.getByText('Remove Property'));
+      fireEvent.click(screen.getByText('Remove Property'));
+      expect(mockHandler).toBeCalledWith({ ...mockProperty });
     });
   });
 });
