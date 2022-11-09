dadef7abc4890c9435ab629e7d2774e0f03fb224
remoteriver <remoteriver@gmail.com>
2022-11-09 14:45:23 +1100

    Add PropertiesView.test

---
+++ src/pages/HomePage/components/PropertiesView/PropertiesView.test.tsx
@@ -0,0 +1,87 @@
+import { render, screen } from '@testing-library/react';
+import PropertiesView from './PropertiesView';
+import PropertyCard from 'components/PropertyCard/PropertyCard';
+
+jest.mock('components/PropertyCard/PropertyCard', () => jest.fn());
+
+describe('PropertiesView', () => {
+  beforeEach(() => {
+    (PropertyCard as jest.Mock).mockImplementation(() => (
+      <div>PropertyCard</div>
+    ));
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('it renders', () => {
+    render(
+      <PropertiesView
+        data={{
+          results: [],
+          saved: [],
+        }}
+      />
+    );
+
+    expect(screen.getByText('Saved Properties')).toBeInTheDocument();
+    expect(screen.getByText('Results')).toBeInTheDocument();
+  });
+
+  test('it renders PropertyCard for saved Properties', () => {
+    const mockProperty = {
+      price: 'price',
+      agency: undefined,
+      id: '1',
+      mainImage: '',
+    };
+
+    render(
+      <PropertiesView
+        data={{
+          results: [],
+          saved: [mockProperty],
+        }}
+      />
+    );
+
+    expect(PropertyCard).toBeCalledWith(
+      {
+        mode: 'remove',
+        property: mockProperty,
+        onButtonClicked: expect.any(Function),
+      },
+      expect.anything()
+    );
+    expect(screen.getAllByText('PropertyCard').length).toBe(1);
+  });
+
+  test('it renders PropertyCard for results', () => {
+    const mockProperty = {
+      price: 'price',
+      agency: undefined,
+      id: '1',
+      mainImage: '',
+    };
+
+    render(
+      <PropertiesView
+        data={{
+          results: [mockProperty],
+          saved: [],
+        }}
+      />
+    );
+
+    expect(PropertyCard).toBeCalledWith(
+      {
+        mode: 'add',
+        property: mockProperty,
+        onButtonClicked: expect.any(Function),
+      },
+      expect.anything()
+    );
+    expect(screen.getAllByText('PropertyCard').length).toBe(1);
+  });
+});
--- src/pages/HomePage/components/PropertiesView/PropertiesView.tsx
+++ src/pages/HomePage/components/PropertiesView/PropertiesView.tsx
@@ -1,12 +1,18 @@
 import { Box, Typography } from '@mui/material';
 import Flex from 'components/Flex/Flex';
 import PropertyCard from 'components/PropertyCard/PropertyCard';
-import data from 'data/data.json';
 import { Property } from 'types/Property';
 import usePropertiesList from '../../hooks/usePropertiesList/usePropertiesList';
 
-const PropertiesView = () => {
-  console.debug(data);
+interface Props {
+  data: {
+    results: Property[];
+    saved: Property[];
+  };
+}
+
+const PropertiesView = (props: Props) => {
+  const { data } = props;
 
   const [results, addResults, removeResults] = usePropertiesList(data.results);
   const [saved, addSaved, removeSaved] = usePropertiesList(data.saved);
@@ -21,9 +27,9 @@ const PropertiesView = () => {
   };
 
   return (
-    <Flex>
+    <Flex p={2}>
       <Box flex={1} px={1}>
-        <Typography>Results</Typography>
+        <Typography variant="h5">Results</Typography>
         {results.map((property) => (
           <PropertyCard
             property={property}
@@ -33,7 +39,7 @@ const PropertiesView = () => {
         ))}
       </Box>
       <Box flex={1} px={1}>
-        <Typography>Saved Properties</Typography>
+        <Typography variant="h5">Saved Properties</Typography>
         {saved.map((property) => (
           <PropertyCard
             property={property}
