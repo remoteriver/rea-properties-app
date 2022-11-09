8d2bd741e6060e407f345cd547af52d02b40268a
remoteriver <remoteriver@gmail.com>
2022-11-09 12:40:46 +1100

    Refactor homepage structure

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,50 +1,10 @@
-import { Container, Box, Typography } from '@mui/material';
-import React, { useState } from 'react';
-import Flex from '../../components/Flex/Flex';
-import PropertyCard from '../../components/PropertyCard/PropertyCard';
-import data from '../../data/data.json';
-import { Property } from '../../types/Property';
-import usePropertiesList from './hooks/usePropertiesList/usePropertiesList';
+import { Container } from '@mui/material';
+import PropertiesView from './components/PropertiesView/PropertiesView';
 
 const Homepage = () => {
-  console.debug(data);
-
-  const [results, addResults, removeResults] = usePropertiesList(data.results);
-  const [saved, addSaved, removeSaved] = usePropertiesList(data.saved);
-
-  const handleAddPropertyToSaved = (property: Property) => {
-    removeResults(property.id);
-    addSaved(property);
-  };
-  const handleRemovePropertyFromSaved = (property: Property) => {
-    removeSaved(property.id);
-    addResults(property);
-  };
-
   return (
     <Container>
-      <Flex>
-        <Box flex={1} px={2}>
-          <Typography>Results</Typography>
-          {results.map((property) => (
-            <PropertyCard
-              property={property}
-              mode="add"
-              onButtonClicked={handleAddPropertyToSaved}
-            />
-          ))}
-        </Box>
-        <Box flex={1}>
-          <Typography>Saved Properties</Typography>
-          {saved.map((property) => (
-            <PropertyCard
-              property={property}
-              mode="remove"
-              onButtonClicked={handleRemovePropertyFromSaved}
-            />
-          ))}
-        </Box>
-      </Flex>
+      <PropertiesView />
     </Container>
   );
 };
